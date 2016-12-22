ca9ef4c45461cceef4aead1a99a8846f09feabe6
JingkaiZhao <jingkai.zhao@foxmail.com>
2016-12-22 20:55:48 +0800

    :bug: disable body parser when request would not be intercepted

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ function rekcom(origin) {
     const _router = require('koa-router')();
     const _app = koa();
     const _intercepters = [];
+    const _interceptRoutes = {};
 
     function _intercept(uri, method) {
         let ic = new Interceptor(uri, method);
+        _interceptRoutes[uri] = true;
         _intercepters.push(ic);
         return ic;
     }
@@ -36,7 +38,13 @@ function rekcom(origin) {
                 let method = _router[ic.method.toLowerCase()];
                 method.call(_router, ic.uri, respond(ic));
             }
-            _app.use(bodyParser())
+
+            _app.use(function* disableBodyParser(next) {
+                    // disable body parser if request would not be intercepted
+                    if (!_interceptRoutes[this.path]) this.disableBodyParser = true;
+                    return yield next;
+                })
+                .use(bodyParser())
                 .use(_router.routes())
                 .use(_router.allowedMethods())
                 .use(proxy({
